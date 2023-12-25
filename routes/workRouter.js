382711f22baa35ee08111f751331140e12100e01
saiBhandarwad <s333bhandarwad@gmail.com>
2023-12-25 14:19:30 +0530

    small changes

--- routes/workRouter.js
+++ routes/workRouter.js
@@ -5,34 +5,47 @@ const workRouter = express.Router()
 const JWT_SEC = process.env.JWT_SEC
 
 workRouter
+    .post("/getAllCityAndWorks", async (req, res) => {
+        try {
+            console.log("hi i am in getallcityandworks");
+            let works = await Work.find()
+            let cityArray = works.map((item) => item.city)
+                .filter((item, index, arr) => arr.indexOf(item) === index)
+            let workTypeArray = works.map((item) => item.type)
+                .filter((item, index, arr) => arr.indexOf(item) === index)
+            res.json({ success: true, cityArray, workTypeArray })
+        } catch (error) {
+            res.json({ success: false, message:error.message })
+        }
+    })
     .post('/getWorksByFilter', async (req, res) => {
         try {
-            
+            console.log(req.body.data);
             if (req.body.data.sortBy) {
                 console.log("one");
                 if (req.body.data.type) {
-                    console.log("two => ",req.body.data.type);
+                    console.log("two => ", req.body.data.type);
                     const works = await Work.find(req.body.data.filterOBJECT).sort
                         ([[req.body.data.sortBy, req.body.data.type]])
-                    res.json({success:true, works })
+                    res.json({ success: true, works })
                     return
-                } 
-                console.log("three => ",req.body.data.sortBy);               
+                }
+                console.log("three => ", req.body.data.sortBy);
                 const works = await Work.find(req.body.data.filterOBJECT).sort
                     ([[req.body.data.sortBy, 'asc']])
-                res.json({success:true, works })
+                res.json({ success: true, works })
                 return
             }
-            
+
             const works = await Work.find(req.body.data.filterOBJECT)
-            res.json({ works })
+            res.json({success: true, works })
         } catch (error) {
-            res.json({ success:false, error: error.message })
+            res.json({ success: false, error: error.message })
         }
     })
     .post('/postJob', async (req, res) => {
         try {
-            const { workType, salary, city, duration, startDate, endDate, detail, address, period,postedDate } = req.body.data
+            const { workType, salary, city, duration, startDate, endDate, detail, address, period, postedDate } = req.body.data
             const { token } = req.body.headers
             const { email } = jwt.verify(token, JWT_SEC)
 
@@ -49,15 +62,15 @@ workRouter
                 user: email,
                 postedDate
             })
-            res.json({success:true, message:"work posted successfully!" ,response})
+            res.json({ success: true, message: "work posted successfully!", response })
         } catch (error) {
-            res.json({success:false, message:"work posting failed" })
+            res.json({ success: false, message: "work posting failed" })
         }
     })
     .post('/works', async (req, res) => {
         try {
-            const works = await Work.find()
-            res.json({ success:true, works })
+            const works = await Work.find().sort([["postedDate","desc"]])
+            res.json({ success: true, works })
         } catch (error) {
             console.log({ error: error.message });
         }
