3b3a3d6d5b5dc70280bbeb412ba73368869a1210
saiBhandarwad <s333bhandarwad@gmail.com>
2024-01-04 15:30:23 +0530

    twillio accounts token changed

--- controller/otpController.js
+++ controller/otpController.js
@@ -3,11 +3,13 @@ const accountSid = process.env.ACCOUNT_SID
 const authToken = process.env.AUTH_TOKEN 
 const client = twilio(accountSid,authToken)
 const twilioPhoneNo = process.env.TWILIO_PHONE_NO
-
 sendOTP = (req,res) => {
 
     const {phone} = req.body.data
     const OTP = Math.round(Math.random()*10000)
+
+    /* OTP sending using twillio */
+
     client.messages.create({
         body:"your one time password for instawork mobile verification is "+OTP,
         from:twilioPhoneNo,
@@ -18,5 +20,7 @@ sendOTP = (req,res) => {
         //console.log({err});
         res.json({err,success:false})
     })
+
+    
 }
 module.exports = sendOTP
