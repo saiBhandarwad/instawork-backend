f6b6fe673951eb81847b4cdc0c118860e4b48f5a
saiBhandarwad <s333bhandarwad@gmail.com>
2024-01-18 13:06:08 +0530

    middlewares added to check mobile or email is already exist before sending message

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const workRouter = require('./routes/workRouter')
 const userRouter = require('./routes/userRouter')
 const connectToMongo = require('./db')
 const emailVerification = require('./controller/emailVerification')
+const User = require('./model/User')
 const server = express()
 const port = process.env.PORT
 
@@ -14,18 +15,41 @@ const port = process.env.PORT
 connectToMongo(process.env.MONGO_URL)
 server.use(express.json())
 server.use(cors({
-    origin:["http://localhost:5173",
-    "https://instawork-job-hunting-site.vercel.app"]
+    origin: ["http://localhost:5173",
+        "https://instawork-job-hunting-site.vercel.app"]
 }))
 
-server.use("/work",auth,workRouter)
-server.use("/user",userRouter)
-server.get("/works",(req,res)=>{
-    console.log({req});
-    let works =  [{type:"painting"},{type:"security"},{type:"data-entry"}]
+server.use("/work", auth, workRouter)
+server.use("/user", userRouter)
+server.get("/works", (req, res) => {
+    console.log({ req });
+    let works = [{ type: "painting" }, { type: "security" }, { type: "data-entry" }]
     res.send(works)
 })
-server.post("/sendOTP",sendOTP)
-server.post("/sendMail",emailVerification)
 
-server.listen(port,()=>console.log('app listning on port number',port))
+const emailCheck = async(req,res,next) => {
+    const {email} = req.body.data
+    const user = await User.find({ email })
+    console.log({user});
+    if(user){
+        res.send({success:false, message:"email already exist"})
+        return
+    }else{
+        next()
+    }
+}
+const mobileCheck = async(req,res,next) => {
+    const {phone} = req.body.data
+    const user = await User.find({ phone })
+    console.log({user});
+    if(user){
+        res.send({success:false, message:"mobile number already exist"})
+        return
+    }else{
+        next()
+    }
+}
+server.post("/sendOTP", mobileCheck, sendOTP)
+server.post("/sendMail", emailCheck, emailVerification)
+
+server.listen(port, () => console.log('app listning on port number', port))
